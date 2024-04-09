2df22d44ce30e7bff2806173e0b53f1d49b68fa2
Gabriel Victor <bielwdev@gmail.com>
2024-04-09 20:16:23 -0300

    created a function to create new attendees

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
+const createNewAttendee = () => {
+    return `
+    <tr>
+        <td>
+            <strong>
+                Gabriel Victor
+            </strong>
+            <br>
+            <small>
+                gabrielvictor@gmail.com
+            </small>
+            </td>
+            <td>há 3 dias</td>
+            <td>há 3 minutos</td>
+        </tr>
+    `
+
+}
+
 const updateList = () => {
-    document.querySelector("tbody").innerHTML = "Testando função"
+    document.querySelector("tbody").innerHTML = createNewAttendee()
 }
 
 updateList()
