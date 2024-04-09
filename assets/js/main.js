26d443f9f46da3c27d33455ea08973a897ffb28e
Gabriel Victor <bielwdev@gmail.com>
2024-04-09 20:20:38 -0300

    created a interpolation on js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,31 @@
-const createNewAttendee = () => {
+const attendee = {
+    nome: "Gabriel Victor",
+    email: "gabriel@gmail.com",
+    dataInscricao: new Date(2024, 3, 8, 17, 17),
+    dataCheckIn: new Date(2024, 3, 9, 20, 12),
+}
+
+const createNewAttendee = (attendee) => {
     return `
     <tr>
         <td>
             <strong>
-                Gabriel Victor
+                ${attendee.nome}
             </strong>
             <br>
             <small>
-                gabrielvictor@gmail.com
+                ${attendee.email}
             </small>
             </td>
-            <td>há 3 dias</td>
-            <td>há 3 minutos</td>
+            <td>${attendee.dataInscricao}</td>
+            <td>${attendee.dataCheckIn}</td>
         </tr>
     `
 
 }
 
-const updateList = () => {
-    document.querySelector("tbody").innerHTML = createNewAttendee()
+const updateList = (attendee) => {
+    document.querySelector("tbody").innerHTML = createNewAttendee(attendee)
 }
 
-updateList()
+updateList(attendee)
